80a85ca656d2be7126dfa41096106a147caca6c6
CHAMIKA DILSHAN HERATH <hmcdilshanherath@gmail.com>
2025-07-30 12:39:36 +0530

    first commit

---
+++ src/test/java/com/example/AtharamanBackend1/AtharamanBackend1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AtharamanBackend1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtharamanBackend1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
