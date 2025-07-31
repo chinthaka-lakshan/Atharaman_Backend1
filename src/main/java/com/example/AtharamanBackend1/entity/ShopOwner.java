02918c4f2d336b99aefac84ed0ca35afac60d993
Visal Hewage <visalsathsara55@gmail.com>
2025-07-31 17:15:21 +0530

    Shop

---
+++ src/main/java/com/example/AtharamanBackend1/entity/ShopOwner.java
@@ -0,0 +1,39 @@
+package com.example.AtharamanBackend1.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "shopowner")
+
+public class ShopOwner {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "shopOwnerName")
+    private String shopOwnerName;
+
+    @Column(name = "nic")
+    private String nic;
+
+    @Column(name = "businessMail")
+    private String businessMail;
+
+    @Column(name = "contactNumber")
+    private String contactNumber;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "userId",referencedColumnName = "id")
+    private User user;
+
+    @OneToMany(mappedBy = "shopOwner")
+    private List<Shop> shops;
+
+
+
+}
--- src/main/java/com/example/AtharamanBackend1/service/LocationService.java
+++
@@ -1,9 +0,0 @@
-package com.example.AtharamanBackend1.service;
-
-import com.example.AtharamanBackend1.dto.LocationDto;
-
-import java.util.List;
-
-public interface LocationService {
-    List<LocationDto> getAllLocations ();
-}
