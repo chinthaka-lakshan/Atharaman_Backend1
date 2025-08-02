31eb8037807c7e69ea023e55091ca1ffba439d30
CHAMIKA DILSHAN HERATH <hmcdilshanherath@gmail.com>
2025-08-02 22:45:27 +0530

    login registration v01

--- src/main/java/com/example/AtharamanBackend1/repository/UserRepository.java
+++ src/main/java/com/example/AtharamanBackend1/repository/UserRepository.java
@@ -1,4 +1,12 @@
 package com.example.AtharamanBackend1.repository;
 
-public interface UserRepository {
+
+import com.example.AtharamanBackend1.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByUsername(String username);
 }
