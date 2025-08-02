d885f5dab652d38aac270dcb226add89d00f3f05
CHAMIKA DILSHAN HERATH <hmcdilshanherath@gmail.com>
2025-08-02 23:45:04 +0530

    login registraton_v02

--- src/main/java/com/example/AtharamanBackend1/dto/UserDto.java
+++ src/main/java/com/example/AtharamanBackend1/dto/UserDto.java
@@ -1,4 +1,20 @@
 package com.example.AtharamanBackend1.dto;
 
+
+import com.example.AtharamanBackend1.entity.Role;
+import com.example.AtharamanBackend1.entity.Token;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class UserDto {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private Role role;
+    private List<Token> tokens;
+
 }
