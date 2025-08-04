c2ca2d3547fa25fa256b56c1a086eb43447ad4e4
CHAMIKA DILSHAN HERATH <hmcdilshanherath@gmail.com>
2025-08-05 01:24:30 +0530

    user request

--- src/main/java/com/example/AtharamanBackend1/service/AuthenticationService.java
+++ src/main/java/com/example/AtharamanBackend1/service/AuthenticationService.java
@@ -6,11 +6,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseEntity;
 
+
 public interface AuthenticationService {
 
     AuthenticationResponse register(UserDto request);
-
     AuthenticationResponse authenticate(UserDto request);
-
     ResponseEntity refreshToken(HttpServletRequest request, HttpServletResponse response);
 }
