89aeddd2ce375feafcadfe9d60d57f0b7a99f8a7
CHAMIKA DILSHAN HERATH <hmcdilshanherath@gmail.com>
2025-08-05 22:47:28 +0530

    requestv01

--- src/main/java/com/example/AtharamanBackend1/service/HotelOwnerService.java
+++ src/main/java/com/example/AtharamanBackend1/service/HotelOwnerService.java
@@ -1,6 +1,8 @@
 package com.example.AtharamanBackend1.service;
 
+import com.example.AtharamanBackend1.dto.GuideRequestDto;
 import com.example.AtharamanBackend1.dto.HotelOwnerDto;
+import com.example.AtharamanBackend1.dto.HotelOwnerRequestDto;
 
 import java.util.List;
 
@@ -10,4 +12,8 @@ public interface HotelOwnerService {
     HotelOwnerDto getHotelOwnerById(Long id);
     HotelOwnerDto updateHotelOwnerById(HotelOwnerDto hotelOwnerDto, Long id);
     void deleteHotelOwnerById(Long id);
+    void submitHotelOwnerRequest(HotelOwnerRequestDto dto);
+    void approveHotelOwnerRequest(Long requestId);
+    List<HotelOwnerRequestDto> getPendingHotelOwnerRequests();
+
 }
